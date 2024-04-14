5a19284ba424455b484a273c49c453bbbee569a6
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-04-14 05:27:42 -0400

    The basic skeleton such as the login and welcome pages with their route.

---
+++ Node_Passport_Login/Routes/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// Login Page
+router.get('/login',(req,res) => {
+    res.send('Login')
+})
+// Register Page
+router.get('/register',(req,res) => {
+    res.send('Register')
+})
+
+module.exports = router;
